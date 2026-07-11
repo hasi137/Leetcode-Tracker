a335b0804c221120b26b3aba25390070b29322aa
hasi137 <vhaseena911@gmail.com>
2026-07-11 12:33:08 +0530

    Create 1444-NumberOfStepsToReduceANumberToZero

---
+++ 1444-NumberOfStepsToReduceANumberToZero/1444-NumberOfStepsToReduceANumberToZero.cpp
@@ -0,0 +1,16 @@
+// Last updated: 11/07/2026, 12:34:05
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int steps = 0;
+        while (num != 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+            steps++; 
+        }
+        return steps;
+    }
+};
