b14f8337e0d80a7156f43374f63a71a4cae637a8
hasi137 <vhaseena911@gmail.com>
2026-07-11 12:32:31 +0530

    Create 1916-FindCenterOfStarGraph

---
+++ 1916-FindCenterOfStarGraph/1916-FindCenterOfStarGraph.java
@@ -0,0 +1,10 @@
+// Last updated: 11/07/2026, 12:33:27
+class Solution {
+    public int findCenter(int[][] edges) {
+        if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) {
+            return edges[0][0];
+        } else {
+            return edges[0][1];
+        }
+    }
+}
