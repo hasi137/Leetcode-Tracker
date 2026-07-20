5aa228960d644427e865665094431c8aa03a1745
hasi137 <vhaseena911@gmail.com>
2026-07-20 23:25:31 +0530

    Create 278-FirstBadVersion

---
+++ 278-FirstBadVersion/version/java/278-FirstBadVersion_20260720_232628.java
@@ -0,0 +1,15 @@
+// Last updated: 20/07/2026, 23:26:28
+1class Solution {
+2    public int numSquares(int n) {
+3        int[] dp = new int[n + 1];
+4
+5        for (int i = 1; i <= n; i++) {
+6            dp[i] = i;
+7            for (int j = 1; j * j <= i; j++) {
+8                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+9            }
+10        }
+11
+12        return dp[n];
+13    }
+14}
